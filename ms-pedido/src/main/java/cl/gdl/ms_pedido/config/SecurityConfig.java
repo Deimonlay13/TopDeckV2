9760cb3699cd2461f6b0da78de5a2c9c9eca4aba
GeraldineBecerra <gera.becerra@duocuc.cl>
2025-07-12 23:38:40 -0400

    se  modifica  script sql se  agrega swagger

--- ms-pedido/src/main/java/cl/gdl/ms_pedido/config/SecurityConfig.java
+++ ms-pedido/src/main/java/cl/gdl/ms_pedido/config/SecurityConfig.java
@@ -13,11 +13,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html")
+                        .permitAll()
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
-                })
-                );
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 
         return http.build();
     }
+
 }
